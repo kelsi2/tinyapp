10770be2836d564a5a9cd6d3de04e885f52661cd
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-01 07:59:15 -0700

    add long URL redirect

--- express_server.js
+++ express_server.js
@@ -35,9 +35,16 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/urls.json", (req, res) => {
