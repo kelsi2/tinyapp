9fdb08240ac60b2ae43943d3a2812c2b9474238c
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-01 17:33:17 -0700

    add edit and delete functionality

--- express_server.js
+++ express_server.js
@@ -31,6 +31,11 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -48,6 +53,16 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  const longURL = req.body.longURL;
+  console.log(longURL);
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls");
+});
+//Cannot POST / urls / http://www.lighthouselabs.ca
+
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
@@ -55,11 +70,6 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
